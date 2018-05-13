294e1510de110bd39fcd96e5e3dd2bcd8dd6fb7c
Otton G Fernandez <ottongabriel@MacBook-Air.local>
2018-05-12 20:57:03 -0400

    fixed title of the page

--- app.js
+++ app.js
@@ -102,7 +102,7 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
 // default value for title local
-app.locals.title = 'ATEysixed';
+app.locals.title = 'ATEySixed';
 
 
 
